fe3053fb998672732f860eb1af332b139b7ddc83
Kantapat-Kwansomkid <72593812+Kantapat-Kwansomkid@users.noreply.github.com>
2021-06-13 16:55:19 +0700

    Add files via upload

---
+++ 1_FinaltermTest_MDT112Workshop.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _1_FinaltermTest_MDT112Workshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("");
+            a = int.Parse(Console.ReadLine());
+
+            int b;
+            Console.Write("");
+            b = int.Parse(Console.ReadLine());
+
+            int c;
+            Console.Write("");
+            c = int.Parse(Console.ReadLine());
+
+            int d;
+            Console.Write("");
+            d = int.Parse(Console.ReadLine());
+
+            int e;
+            Console.Write("");
+            e = int.Parse(Console.ReadLine());
+
+            int f;
+            Console.Write("");
+            f = int.Parse(Console.ReadLine());
+
+            int g;
+            Console.Write("");
+            g = int.Parse(Console.ReadLine());
+
+            int h;
+            Console.Write("");
+            h = int.Parse(Console.ReadLine());
+
+            int i;
+            Console.Write("");
+            i = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0} {1} {2}", a, b, c);
+            Console.WriteLine("{0} {1} {2}", d, e, f);
+            Console.WriteLine("{0} {1} {2}", g, h, i);
+
+            Console.ReadLine();
+        }
+    }
+}
